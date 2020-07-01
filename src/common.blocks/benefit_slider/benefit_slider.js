bf124c5e9f9b7765223a605de09d92570d8f1453
Tasya <den1647ko@mail.ru>
2020-07-01 13:18:23 +0300

    starting state of project - created landing-page, added header, callback-for and slider to it

---
+++ src/common.blocks/benefit_slider/benefit_slider.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function(){
+    $('.benefit_slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 2,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+    });
+});
---
+++ src/pages/landing-page/landing-page.js
@@ -0,0 +1,4 @@
+import '../../common.blocks/header/header.js'
+import '../../common.blocks/benefit_slider/benefit_slider.js'
+import '../../main.less'
+import './landing-page.less'
